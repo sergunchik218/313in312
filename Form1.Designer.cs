a5cd9a39b88a8089bc774e080b38927e5e7e524a
ThorZol <58010547+sergunchik218@users.noreply.github.com>
2023-08-16 10:15:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace _313_in_220
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.but1 = new System.Windows.Forms.Button();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.but2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(12, 70);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(139, 23);
+            this.but1.TabIndex = 0;
+            this.but1.Text = "Выбираем 313 отчет";
+            this.but1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.but1.UseVisualStyleBackColor = false;
+            this.but1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lable1
+            // 
+            this.lable1.AutoSize = true;
+            this.lable1.Location = new System.Drawing.Point(215, 77);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(0, 15);
+            this.lable1.TabIndex = 1;
+            // 
+            // but2
+            // 
+            this.but2.Location = new System.Drawing.Point(193, 308);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(165, 32);
+            this.but2.TabIndex = 2;
+            this.but2.Text = "Волшебная кнопка";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(966, 602);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 594);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.lable1);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Label lable1;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace _313_in_220
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XDocument text1;
+        XDocument text2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "XML-файл |*.xml|All files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lable1.Text = openFileDialog1.FileName;
+                text1 = XDocument.Load(lable1.Text);
+            }
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            //saveFileDialog1.Filter = "XML-файл |*.xml|All files|*.*";
+            if (folderDlg.ShowDialog() == DialogResult.OK)
+            {
+                int l1 = 1;
+             
+                foreach (var o1 in text1.Descendants()?.Elements("sscc"))
+                {
+                    text2 = new XDocument();
+                    XElement doc = new XElement("documents");
+                    doc.Add(new XAttribute("session_ui", "4cbec96e-67f2-2465-e055-025056ad03aa"));
+                    doc.Add(new XAttribute("version", "1.35"));
+                    doc.Add(new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"));
+                    XElement quer = new XElement("query_hierarchy_info");
+                    quer.Add(new XAttribute("action_id", "220"));
+                    quer.Add(new XElement("subject_id", "00000000191233"));
+                    quer.Add(new XElement("sscc", o1.Value));
+                    doc.Add(quer);
+                    text2.Add(doc);
+                    text2.Save(folderDlg.SelectedPath + "/220_" + l1 + ".xml");
+                    l1++;
+                }
+            }
+        }
+
+    }
+}
